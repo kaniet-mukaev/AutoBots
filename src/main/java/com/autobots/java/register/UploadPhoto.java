447d50bff7fecc70694414b4ad95b2ec6305bfba
Kaniet <mukaev.kaniet@gmail.com>
2025-07-07 09:36:55 +0600

    07/07/2025

---
+++ src/main/java/com/autobots/java/register/UploadPhoto.java
@@ -0,0 +1,7 @@
+package com.autobots.java.register;
+
+public class UploadPhoto {
+    public static void main(String[] args) {
+        System.out.println("Upload photo");
+    }
+}
--- src/main/java/com/autobots/java/sport/FootBall.java
+++ src/main/java/com/autobots/java/sport/FootBall.java
@@ -2,3 +2,4 @@ package com.autobots.java.sport;
 
 public class FootBall {
 }
+
