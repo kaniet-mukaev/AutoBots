bae7bd15783fca94f5066cb6526eeaf2a6310406
Kaniet <mukaev.kaniet@gmail.com>
2025-07-08 09:23:51 +0600

    vers 1.0.2

--- src/main/java/ResetDemo.java
+++ src/main/java/ResetDemo.java
@@ -2,5 +2,6 @@ public class ResetDemo {
     public static void main(String[] args) {
         System.out.println("v1.0.0");
         System.out.println("v1.0.1");
+        System.out.println("v1.0.2");
     }
 }
